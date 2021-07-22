e8c3d3b5a66c97d2172760400750d58607639b12
noh-yj <goky456@gmail.com>
2021-07-22 23:36:08 +0900

    upload

--- App.js
+++ App.js
@@ -1,21 +1,154 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  Alert,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>나만의 꿀팁</Text>
+      <Image
+        source={{
+          uri: 'https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fmain.png?alt=media&token=8e5eb78d-19e',
+        }}
+        style={styles.mainImage}
+      />
+      <ScrollView
+        style={styles.middleContainer}
+        horizontal
+        indicatorStyle={'white'}
+      >
+        <TouchableOpacity style={styles.middleButton01}>
+          <Text style={styles.middleButtonText}>생활</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton02}>
+          <Text style={styles.middleButtonText}>재테크</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton03}>
+          <Text style={styles.middleButtonText}>반려견</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton04}>
+          <Text style={styles.middleButtonText}>꿀팁 찜</Text>
+        </TouchableOpacity>
+      </ScrollView>
+      <View style={styles.cardContainer}>
+        <View style={styles.card}>
+          <Image
+            source={{
+              url: 'https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d8',
+            }}
+            style={styles.cardImage}
+          />
+        </View>
+        <View style={styles.cardText}>
+          <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게</Text>
+          <Text style={styles.cardDesc} numberOfLines={3}>
+            먹다 남은 피자는 수분이 날라가기 때문에 처음처럼 먹을 수
+            없는데요.처음처럼 먹을 수 없는데요.처음처럼 먹을 수
+            없는데요.처음처럼 먹을 수 없는데요.처음처럼 먹을 수
+            없는데요.처음처럼 먹을 수 없는데요.처음처럼 먹을 수 없는데요.
+          </Text>
+          <Text style={styles.cardDate}>2020.09.09</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginLeft: 20,
+  },
+  mainImage: {
+    width: '90%',
+    height: 200,
+    borderRadius: 10,
+    marginTop: 20,
+    alignSelf: 'center',
+  },
+  middleContainer: {
+    marginTop: 20,
+    marginLeft: 10,
+    height: 60,
+  },
+  middleButton01: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: '#fdc453',
+    borderColor: 'deeppink',
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButton02: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: '#fe8d6f',
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButton03: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: '#9adbc5',
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButton04: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: '#f886a8',
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  cardContainer: { marginTop: 10, marginLeft: 10 },
+  card: {
+    flex: 1,
+    flexDirection: 'row',
+    margin: 10,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#eee',
+    paddingBottom: 10,
+  },
+  cardImage: {
+    flex: 1,
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  cardText: {
+    flex: 2,
+    flexDirection: 'column',
+    marginLeft: 10,
+  },
+  cardTitle: {
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  cardDesc: { fontSize: 15 },
+  cardDate: {
+    fontSize: 10,
+    color: '#A6A6A6',
   },
 });
